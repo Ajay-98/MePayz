048575df5add642214c7b93e4d5f769244b0f359
Ajay-98 <ajayelectricalengineer2@gmail.com>
2023-07-09 12:00:37 +0530

    Adding SQL Alchemy grouping logic
    Signed-off-by: Ajay-98 <ajayelectricalengineer2@gmail.com>

--- Src/Connectors/Execute_Plotter.py
+++ Src/Connectors/Execute_Plotter.py
@@ -1,26 +1,28 @@
-from sqlalchemy import Column, String, Date, Float, create_engine
-from sqlalchemy.orm import Session, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Date, Float, create_engine, func
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 Base = declarative_base()
 URI = 'mssql+pyodbc://DESKTOP-M0S2O65/blop?driver=SQL+Server'
 
+
 class stmt_info(Base):
     __tablename__ = "STMT_Info"
 
-    date_ = Column("dated_on", Date, primary_key = True)
+    date_ = Column("dated_on", Date, primary_key=True)
     paid_to = Column("paid_to", String)
     stmt_desc = Column("Stmt_desc", String)
     balance_amt = Column("balance_amt", Float)
+    expenditure = Column("expenditure", Float)
 
-    def __int__(self, date, paid_to, stmt_desc, balance_amt):
+    def __int__(self, date, paid_to, stmt_desc, balance_amt, expenditure):
         date_ = self.date
         paid_to = self.paid_to
         stmt_desc = self.stmt_desc
         balance_amt = self.balance_amt
+        expenditure = self.expenditure
 
 # Initialize MSSQL DB connection.
-engine_ = create_engine(URI, echo = True)
+engine_ = create_engine(URI, echo=True, use_setinputsizes=False)
 Base.metadata.create_all(bind=engine_)
 
 Session = sessionmaker(bind=engine_)
@@ -30,8 +32,8 @@ session = Session()
 get_all = session.query(stmt_info).all()
 
 # Get grouped balance mat, for each dates.
-get_grouped = session.query(stmt_info).select_from(stmt_info.date_).group_by(stmt_info.date_).all()
-for i in get_grouped:
-    print(i.date_)
+# Select dated_on, SUM(expenditure) as Day_Balance from STMT_Info group BY dated_on
+get_grouped = session.query(stmt_info.date_, func.sum(stmt_info.expenditure)).group_by(stmt_info.date_).all()
+print(get_grouped)
 
 session.close_all()
